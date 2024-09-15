69ae04ec86ae2e71c5c42acd23a0988cdf5c7853
Rishesh <shakyarishesh@gmail.com>
2024-09-15 23:08:20 +0545

    fixed image src for logo.png

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Header</title>
-    <link rel="stylesheet" href="css/intro.css">
+    <link rel="stylesheet" href="{{ asset('css/intro.css') }}">
 </head>
 
 <body>
     <header>
-        <a href="#" class="logo"><img src="Images/logo.png" alt="logo"></a>
+        <a href="#" class="logo"><img src="{{asset('Images/logo.png')}}" alt="logo"></a>
         <div class="bx bx-menu" id="menu-icon"></div>
         <ul class="navbar">
             <li><a href="/">Home</a></li>
